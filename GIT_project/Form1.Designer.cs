d68d31705c795a02287d2eec96302cfe1acb5edb
Max <vusykmaxim@gmail.com>
2026-01-16 23:31:32 +0200

    Added ability to add, execute, and delete tasks

--- GIT_project/Form1.Designer.cs
+++ GIT_project/Form1.Designer.cs
@@ -34,20 +34,18 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             dateTimePicker1 = new DateTimePicker();
-            comboBox1 = new ComboBox();
             dataGridView1 = new DataGridView();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(80, 125);
+            button1.Location = new Point(80, 159);
             button1.Name = "button1";
-            button1.Size = new Size(300, 29);
+            button1.Size = new Size(350, 29);
             button1.TabIndex = 0;
             button1.Text = "add task";
             button1.UseVisualStyleBackColor = true;
@@ -55,9 +53,9 @@
             // 
             // button2
             // 
-            button2.Location = new Point(80, 388);
+            button2.Location = new Point(80, 400);
             button2.Name = "button2";
-            button2.Size = new Size(151, 29);
+            button2.Size = new Size(170, 29);
             button2.TabIndex = 1;
             button2.Text = "done";
             button2.UseVisualStyleBackColor = true;
@@ -65,9 +63,9 @@
             // 
             // button3
             // 
-            button3.Location = new Point(237, 388);
+            button3.Location = new Point(260, 400);
             button3.Name = "button3";
-            button3.Size = new Size(139, 29);
+            button3.Size = new Size(170, 29);
             button3.TabIndex = 2;
             button3.Text = "delete";
             button3.UseVisualStyleBackColor = true;
@@ -75,46 +73,40 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(282, 21);
+            textBox1.Location = new Point(180, 56);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(250, 27);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(282, 54);
+            textBox2.Location = new Point(180, 93);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(250, 27);
             textBox2.TabIndex = 4;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(229, 87);
+            dateTimePicker1.Location = new Point(180, 126);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(250, 27);
             dateTimePicker1.TabIndex = 5;
             // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(229, 160);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 6;
-            // 
             // dataGridView1
             // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(80, 194);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(350, 200);
             dataGridView1.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(80, 24);
+            label1.Location = new Point(80, 59);
             label1.Name = "label1";
             label1.Size = new Size(52, 20);
             label1.TabIndex = 8;
@@ -123,7 +115,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(80, 57);
+            label2.Location = new Point(80, 96);
             label2.Name = "label2";
             label2.Size = new Size(88, 20);
             label2.TabIndex = 9;
@@ -132,32 +124,21 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(80, 92);
+            label3.Location = new Point(80, 131);
             label3.Name = "label3";
             label3.Size = new Size(72, 20);
             label3.TabIndex = 10;
             label3.Text = "Deadline:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(82, 163);
-            label4.Name = "label4";
-            label4.Size = new Size(50, 20);
-            label4.TabIndex = 11;
-            label4.Text = "label4";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
-            Controls.Add(comboBox1);
             Controls.Add(dateTimePicker1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -180,11 +161,9 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private DateTimePicker dateTimePicker1;
-        private ComboBox comboBox1;
         private DataGridView dataGridView1;
         private Label label1;
         private Label label2;
         private Label label3;
-        private Label label4;
     }
 }
--- GIT_project/Form1.cs
+++ GIT_project/Form1.cs
@@ -1,7 +1,13 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
 namespace GIT_project
 {
     public partial class Form1 : Form
     {
+        string connectionString =
+    "Server=(localdb)\\MSSQLLocalDB;Database=SQLdb;Trusted_Connection=True;";
         public Form1()
         {
             InitializeComponent();
@@ -9,24 +15,87 @@ namespace GIT_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            LoadTasks();
         }
 
         //btn add task
         private void button1_Click(object sender, EventArgs e)
         {
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
 
+            string sql = @"INSERT INTO Tasks (Title, Description, Deadline)
+                   VALUES (@Title, @Description, @Deadline)";
+
+            using SqlCommand cmd = new SqlCommand(sql, conn);
+
+            cmd.Parameters.AddWithValue("@Title", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Description",
+                string.IsNullOrWhiteSpace(textBox2.Text)
+                    ? (object)DBNull.Value
+                    : textBox2.Text);
+
+            cmd.Parameters.AddWithValue("@Deadline", dateTimePicker1.Value);
+
+            cmd.ExecuteNonQuery();
+
+            LoadTasks();
+            MessageBox.Show("Завдання додано");
+            
         }
+
+        private void LoadTasks()
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            string sql = "SELECT * FROM Tasks";
+
+            using SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
 
         //done
         private void button2_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Id"].Value);
+
+                using SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string sql = "UPDATE Tasks SET IsCompleted = 1 WHERE Id = @Id";
+                using SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadTasks();
 
         }
 
         //delete
         private void button3_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Id"].Value);
+
+                using SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                string sql = "DELETE FROM Tasks WHERE Id = @Id";
+                using SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadTasks();
 
         }
     }
