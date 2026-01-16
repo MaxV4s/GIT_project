b3a69bbe82f2177dbac8aa51dc9be9ede4c5f1b5
Admin <Admin@DESKTOP-PG59NQM>
2026-01-16 20:28:09 +0200

    Add project files.

---
+++ GIT_project/Form1.cs
@@ -0,0 +1,33 @@
+namespace GIT_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //add task
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //done
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //delete
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
